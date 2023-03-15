a2a426b55981ca2fb4867637dab76e7e789e80d9
lstubbs12 <jakestubbs16@gmail.com>
2023-03-15 10:49:01 -0500

    New keyboard and chartdistance
    Hitboxes adjusted also

--- VR Test/Assets/Scripts/ChartDistance.cs
+++ VR Test/Assets/Scripts/ChartDistance.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class ChartDistance : MonoBehaviour
 {
-
+    Vector3 startPos;
+    public GameObject forward;
     private void Start() {
-        
+        startPos = transform.position;
     }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.DownArrow)){
-            transform.position += transform.right;
+            transform.position += forward.transform.right;
         }
         if(Input.GetKeyDown(KeyCode.UpArrow)){
-            transform.position -= transform.right;
+            transform.position -= forward.transform.right;
+        }
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            transform.position = startPos;
         }
     }
     
